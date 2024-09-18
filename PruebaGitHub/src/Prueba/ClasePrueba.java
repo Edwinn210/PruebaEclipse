4f61a8e6003df6023aa3627d24ac228c794ad30e
Edwinn210 <edwinn01@gmail.com>
2024-09-18 18:40:53 -0500

    Se agrega la segunda linea

--- PruebaGitHub/src/Prueba/ClasePrueba.java
+++ PruebaGitHub/src/Prueba/ClasePrueba.java
@@ -5,7 +5,7 @@ public class ClasePrueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");	
-		
+		System.out.println("Esta es la segunda prueba de que puedo modificar el codigo");
 	}
 
 }
